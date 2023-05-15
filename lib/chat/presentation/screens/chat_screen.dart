7927806069cb2b823e655c05c4962532cb642e8a
alexroa <alexroa@krasamo.com>
2023-05-15 16:00:31 -0600

    Added some logic for openAI integration

--- lib/chat/presentation/screens/chat_screen.dart
+++ lib/chat/presentation/screens/chat_screen.dart
@@ -1,5 +1,8 @@
+import 'package:chat_gpt_app/chat/presentation/chat_gpt_cubit.dart';
+import 'package:chat_gpt_app/chat/presentation/chat_gpt_cubit.dart';
 import 'package:chat_gpt_app/chat/presentation/widgets/textfield_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../widgets/chat_message_widget.dart';
 
@@ -16,17 +19,58 @@ class ChatScreen extends StatelessWidget {
       body: SafeArea(
         child: Column(
           children: [
-            Flexible(
-              child: ListView.builder(
-                itemCount: 50,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Container(
-                    height: 50,
-                    color: Colors.red,
-                  ),
-                ),
-              ),
+            BlocBuilder<ChatGptCubit, ChatGptState>(
+              builder: (context, state) {
+                if (state is ChatGptInitial) {
+                  return Flexible(
+                    child: ListView.builder(
+                      itemCount: 0,
+                      itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Container(
+                          height: 50,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ),
+                  );
+                }
+                if (state is ChatGptInsert) {
+                  return Flexible(
+                    child: ListView.builder(
+                      itemCount: state.messages.length,
+                      itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: SizedBox(
+                          height: 50,
+                          child: Text(state.messages[index]!),
+                        ),
+                      ),
+                    ),
+                  );
+                }
+                if (state is ChatGptLoadingRequest) {
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                if (state is ChatGptRequestResponse) {
+                  return Flexible(
+                    child: ListView.builder(
+                      itemCount: state.updatedList.length,
+                      itemBuilder: (context, index) => Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: SizedBox(
+                          height: 50,
+                          child: Text(state.updatedList[index]!),
+                        ),
+                      ),
+                    ),
+                  );
+                } else {
+                  return Container();
+                }
+              },
             ),
             TextFieldWidget(controller: _textEditingController),
           ],
